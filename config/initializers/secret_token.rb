2ddf5e2d97afa97fb0a6a34f4832a5799c356342
Juan Posadas <jposadas@Juan-Posadass-MacBook-Pro.local>
2013-07-18 14:28:55 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HermanosScholarship::Application.config.secret_token = '1b9e720b18a69c3b4b5b4120fedfb72e0a566b2af504a160ae2340e8fdeb5de34773d5f9d285e13aedd58f819034f85af37d78cffc5f8a79c0ad769a97ea3038'
